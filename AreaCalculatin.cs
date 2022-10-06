7c46cf31bc96154816b6cdcf34c752fbc6a6168b
ImDoctorWho <48880085+ImDoctorWho@users.noreply.github.com>
2022-10-06 05:39:40 +0500

    Добавьте файлы проекта.

---
+++ AreaCalculatin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+
+namespace TestLibrary
+{
+    public static class AreaCalculatin
+    { 
+        /// <summary>
+        /// Высчитывает площадь круга по радиусу 
+        /// </summary>
+        /// <param name="r">Значение радиуса круга</param>
+        /// <returns>Возвращает значение площади круга</returns>
+        /// <returns>Если радиус равен отрицательному числу, то будет возвращен 0</returns>
+        public static double CircleArea(int r)
+        {
+            //Возвращает 0 если радиус отрицательный
+            if (r < 0)
+            {
+                return 0;
+            }
+            //S = π × r2
+            return Math.Round(Math.PI * Math.Pow(r, 2), 2);
+        }
+
+        /// <summary>
+        /// Высчитывает площать треугольника по формуле Герона
+        /// </summary>
+        /// <param name="a">Значение стороны A</param>
+        /// <param name="b">Значение стороны B</param>
+        /// <param name="c">Значение стороны C</param>
+        /// <returns>Возвращает значение площади треугольника</returns>
+        /// <returns>Если треугольник неправильный, то будет возвращен 0</returns>
+        public static double TriangleArea(int a, int b, int c)
+        {
+            //Проверка треугольника на правильность 
+            if ( (a + b) < c | (a + c) < b | (c + b) < a )
+            {
+                return 0;
+            }
+
+            //Расчет полуперметра
+            //1/2(a+b+c)
+            double p = 0.5 * (a + b + c);
+           
+
+            //S = √p(p-a)(p-b)(p-c)
+            return Math.Round(Math.Sqrt(p*(p-a)*(p-b)*(p-c)), 2);
+        }
+
+       
+
+    }
+}
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLibrary;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void AreaCircleTest1()
+        {
+            int radius = 5;
+            double excpected = 78.54;
+            double result = AreaCalculatin.CircleArea(radius);
+            Assert.AreEqual(excpected, result, "Test is not Correct");
+        }
+
+        [TestMethod]
+        public void AreaCircleTest2()
+        {
+            int radius = 50;
+            double excpected = 7853.98;
+            var result = AreaCalculatin.CircleArea(radius);
+            Assert.AreEqual(excpected, result, "Test is not Correct");
+        }
+
+        [TestMethod]
+        public void AreaCircleTest3()
+        {
+            int radius = -999;
+            double excpected = 0;
+            var result = AreaCalculatin.CircleArea(radius);
+            Assert.AreEqual(excpected, result, "Test is not Correct");
+        }
+
+        [TestMethod]
+        public void AreaTriangle1()
+        {
+            int a = 5;
+            int b = 5;
+            int c = 5;
+            double excpected = 10.83;
+            var result = AreaCalculatin.TriangleArea(a, b, c);
+            Assert.AreEqual(excpected, result, "Test is not Correct");
+        }
+
+        [TestMethod]
+        public void AreaTriangle2()
+        {
+            int a = 20;
+            int b = 20;
+            int c = 20;
+            double excpected = 173.21;
+            var result = AreaCalculatin.TriangleArea(a, b, c);
+            Assert.AreEqual(excpected, result, "Test is not Correct");
+        }
+
+        [TestMethod]
+        public void AreaTriangle3()
+        {
+            int a = 150;
+            int b = 100;
+            int c = 1;
+            double excpected = 0;
+            var result = AreaCalculatin.TriangleArea(a, b, c);
+            Assert.AreEqual(excpected, result, "Test is not Correct");
+        }
+
+
+
+
+    }
+}
